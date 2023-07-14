e2df059f1f24dc38a6854631ad0e1e2344e530ca
getssh <gtamrat33@gmail.com>
2023-07-14 15:44:14 +0300

    Add public/private toggle feature to recipe show

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   scope :public_recipes, -> { where(public: true).order(created_at: :desc) }
+  attribute :public, :boolean, default: false
 end
